3ab1936f0cd931e5689306b71f72e61f63137512
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2020-01-11 20:10:01 -0300

    - Criado component CardsStack

---
+++ src/components/CardsStack/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {CardDeck, Card } from 'react-bootstrap'
+import imgNode from '../../assets/nodejs.png'
+import imgJava from '../../assets/java.jpeg'
+import imgDelphi from '../../assets/delphi.jpg'
+
+export default function CardsStack() {
+  return (
+    <CardDeck>
+      <Card>
+        <Card.Img variant="top" src={imgNode} />
+        <Card.Body>
+          <Card.Title>NodeJS</Card.Title>
+          <Card.Text>
+            <li>APIs Com typescript e express</li>
+            <li>Frontend com reactJS</li>
+            <li>Mobile com react native</li> 
+            <li>Banco de dados mysql e postgresSQL com sequelize</li>
+            <li>banco de dados mongoDB com mongoose</li>
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={imgJava} />
+        <Card.Body>
+          <Card.Title>Java</Card.Title>
+          <Card.Text>
+            <li>JavaEE com jsf e primefaces.</li>
+            <li>spring e thymeleaf</li>
+            <li>JPA e hibernat</li>  
+            <li>Relatórios com iReport</li> 
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src={imgDelphi} />
+        <Card.Body>
+          <Card.Title>Delphi 2009</Card.Title>
+          <Card.Text>
+            <li>Aplicações desktop com delphi 2009</li>
+            <li>Relatórios com iReport</li>
+            <li>Banco de dados mysql.</li>
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+        </Card.Footer>
+      </Card>
+    </CardDeck>
+  )
+}

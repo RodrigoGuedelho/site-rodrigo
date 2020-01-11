40e8669dc9707e81a3ab4e361c3676cd23794690
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2020-01-11 20:08:25 -0300

    - Criado componente NavBar;

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap'
+
+// import { Container } from './styles';
+
+  export default function NavbBar() {
+
+    return (   
+      <Navbar  bg="dark" variant="dark">
+        <Navbar.Brand href="/"><br/></Navbar.Brand>
+      </Navbar> 
+    );
+};
+
+

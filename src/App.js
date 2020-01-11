8ee18ffc3bac0dc7ec89d34f8008fb68fbf94c8e
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2020-01-11 20:10:36 -0300

    - Pagina principal e arquivo de estilização;

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import Title from './components/Title'
+import CardsStack from './components/CardsStack'
+import CardAbout from './components/CardAbout'
+
+import {Row, Col, Image, Container} from 'react-bootstrap'
+
+import './style.css'
+
+
+function App() {
+  return (
+    <>
+      <NavBar />
+        <Container>
+        <br /> 
+        <Title />
+        <br /> <br />
+        <center >
+          <h3>Stacks de desenvolvimento:</h3>
+        </center>
+        <hr style = {{backgroundColor: '#fff'}} />
+        <CardsStack />
+        <br /> <br /> 
+        <CardAbout />
+        </Container>
+        <br /> <br /> 
+      <Footer />
+    </>
+  );
+}
+
+export default App;
